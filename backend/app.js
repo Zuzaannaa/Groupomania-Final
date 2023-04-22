1ca8b3b56867f6780c18e694e4d76794e704de2d
ZuzaL <zuzalubb@gmail.com>
2023-04-21 21:07:59 -0600

    app file

--- backend/app.js
+++ backend/app.js
@@ -1,48 +1,43 @@
-const http = require("http");
-const app = require("./app");
-require("dotenv").config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const path = require(`path`);
+const helmet = require("helmet");
+const cors = require("cors");
 
-const normalizePort = (val) => {
-  const port = parseInt(val, 10);
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
-};
+const userRoutes = require("./routes/user");
+const postRoutes = require("./routes/post");
+const commentRoutes = require("./routes/comment");
 
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
+const db = require("./models/index");
 
-const errorHandler = (error) => {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-  const address = server.address();
-  const bind =
-    typeof address === "string" ? "pipe " + address : "port: " + port;
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges.");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use.");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
+db.sequelize.sync();
 
-const server = http.createServer(app);
+app.use(cors());
 
-server.on("error", errorHandler);
-server.on("listening", () => {
-  const address = server.address();
-  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
-  console.log("Listening on " + bind);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
-server.listen(port);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use(express.json());
+
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
+
+app.use(helmet());
+
+module.exports = app;
