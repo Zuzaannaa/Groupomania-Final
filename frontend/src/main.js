d3480a4c32304bab4cdbc232c6c0c85c089957b6
ZuzaL <zuzalubb@gmail.com>
2023-04-22 20:50:01 -0600

    add images to assets

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store";
+import router from "@/router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
-import './assets/main.css'
-
-createApp(App).mount('#app')
+createApp(App).use(store).use(router).mount("#app");
